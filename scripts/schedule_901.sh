f60bf68f890c2cbde55fb99dc3a71b52328b5573
Arthur Dujardin <arthurdujardin.dev@gmail.com>
2025-08-13 17:36:18 +0000

    fix scripts

--- scripts/schedule_901.sh
+++ scripts/schedule_901.sh
@@ -1,6 +1,7 @@
 # This script is used to schedule the execution of multiple experiments.
-CUDA_VISIBLE_DEVICES=0 python scripts/02_train.py --dataset-id 901 --fold 0
-CUDA_VISIBLE_DEVICES=0 python scripts/02_train.py --dataset-id 901 --fold 1
-CUDA_VISIBLE_DEVICES=0 python scripts/02_train.py --dataset-id 901 --fold 2
-CUDA_VISIBLE_DEVICES=0 python scripts/02_train.py --dataset-id 901 --fold 3
-CUDA_VISIBLE_DEVICES=0 python scripts/02_train.py --dataset-id 901 --fold 4
+python scripts/02_plan.py --dataset-id 901 -y
+CUDA_VISIBLE_DEVICES=0 python scripts/02_train.py --dataset-id 901 --fold 0 -y --plan nnUNetResEncUNetLPlans
+CUDA_VISIBLE_DEVICES=0 python scripts/02_train.py --dataset-id 901 --fold 1 -y --plan nnUNetResEncUNetLPlans
+CUDA_VISIBLE_DEVICES=0 python scripts/02_train.py --dataset-id 901 --fold 2 -y --plan nnUNetResEncUNetLPlans
+CUDA_VISIBLE_DEVICES=0 python scripts/02_train.py --dataset-id 901 --fold 3 -y --plan nnUNetResEncUNetLPlans
+CUDA_VISIBLE_DEVICES=0 python scripts/02_train.py --dataset-id 901 --fold 4 -y --plan nnUNetResEncUNetLPlans
--- scripts/schedule_902.sh
+++ scripts/schedule_902.sh
@@ -1,6 +1,6 @@
 # This script is used to schedule the execution of multiple experiments.
-CUDA_VISIBLE_DEVICES=0 python scripts/02_train.py --dataset-id 902 --fold 0
-CUDA_VISIBLE_DEVICES=0 python scripts/02_train.py --dataset-id 902 --fold 1
-CUDA_VISIBLE_DEVICES=0 python scripts/02_train.py --dataset-id 902 --fold 2
-CUDA_VISIBLE_DEVICES=0 python scripts/02_train.py --dataset-id 902 --fold 3
-CUDA_VISIBLE_DEVICES=0 python scripts/02_train.py --dataset-id 902 --fold 4
+CUDA_VISIBLE_DEVICES=1 python scripts/02_train.py --dataset-id 902 --fold 0 -y --plan nnUNetResEncUNetLPlans
+CUDA_VISIBLE_DEVICES=1 python scripts/02_train.py --dataset-id 902 --fold 1 -y --plan nnUNetResEncUNetLPlans
+CUDA_VISIBLE_DEVICES=1 python scripts/02_train.py --dataset-id 902 --fold 2 -y --plan nnUNetResEncUNetLPlans
+CUDA_VISIBLE_DEVICES=1 python scripts/02_train.py --dataset-id 902 --fold 3 -y --plan nnUNetResEncUNetLPlans
+CUDA_VISIBLE_DEVICES=1 python scripts/02_train.py --dataset-id 902 --fold 4 -y --plan nnUNetResEncUNetLPlans

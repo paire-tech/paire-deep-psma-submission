ab2e0c4bc5bdbfb8cad32f18f87d8214bf54cf8b
Arthur Dujardin <arthurdujardin.dev@gmail.com>
2025-08-13 15:38:19 +0000

    add single plan script and some schedule stuff

---
+++ scripts/schedule_exp802.sh
@@ -0,0 +1,6 @@
+# This script is used to schedule the execution of multiple experiments.
+CUDA_VISIBLE_DEVICES=0 python scripts/02_train.py --dataset-id 802 --fold 0
+CUDA_VISIBLE_DEVICES=0 python scripts/02_train.py --dataset-id 802 --fold 1
+CUDA_VISIBLE_DEVICES=0 python scripts/02_train.py --dataset-id 802 --fold 2
+CUDA_VISIBLE_DEVICES=0 python scripts/02_train.py --dataset-id 802 --fold 3
+CUDA_VISIBLE_DEVICES=0 python scripts/02_train.py --dataset-id 802 --fold 4

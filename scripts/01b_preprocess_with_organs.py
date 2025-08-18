14dc5df35d6a7ed3cec7570d2636f6c6a1c4737e
Arthur Dujardin <arthurdujardin.dev@gmail.com>
2025-08-18 12:55:56 +0200

    reduce tau

--- scripts/01b_preprocess_with_organs.py
+++ scripts/01b_preprocess_with_organs.py
@@ -217,12 +217,12 @@ def main() -> None:
                     useImageSpacing=True,
                 )
                 # Normalize the SDF to [0, 1] using a sigmoid function
-                # We parametrize the sigmoid 𝜎 with τ = w / 2.197 because:
+                # We parametrize the sigmoid 𝜎 with τ = w / 4.394 because:
                 # - At x = +2.197, the sigmoid 𝜎(x) ≈ 0.9.
                 # - At x = -2.197, the sigmoid 𝜎(x) ≈ 0.1.
                 # -> The total distance in x-space between [0.1, 0.9] is therefore about ≈ 4.394,
                 # That's why τ = w / 4.394 provides a sigmoid that smoothly transitions between 0.1 and 0.9.
-                w_mm = 100.0
+                w_mm = 10.0
                 tau = w_mm / 4.394
                 organ_mask_image = 1 / (1 + sitk.Exp(-organ_mask_image / tau))
 

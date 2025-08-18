0cc86e3b924a2fa51f4611b999faf6d745cd3e8b
Arthur Dujardin <arthurdujardin.dev@gmail.com>
2025-08-18 13:31:55 +0200

    parallel nnunet

--- scripts/01b_preprocess_with_organs.py
+++ scripts/01b_preprocess_with_organs.py
@@ -195,7 +195,16 @@ def main() -> None:
 
     # Run nnUNet preprocessing command to preprocess the dataset
     subprocess.run(
-        ["nnUNetv2_plan_and_preprocess", "-d", str(args.dataset_id), "-c", "3d_fullres", "--verify_dataset_integrity"],
+        [
+            "nnUNetv2_plan_and_preprocess",
+            "-d",
+            str(args.dataset_id),
+            "-c",
+            "3d_fullres",
+            "-np",
+            str(args.workers),
+            "--verify_dataset_integrity",
+        ],
         check=True,
     )
     print(f"Preprocessing completed for dataset {dataset_name}.")
